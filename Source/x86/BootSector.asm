1a213ffc24dfc21e418c1ce97c3103e29c39b228
ElectronWrangler <72363575+ElectronWranglerTV@users.noreply.github.com>
2020-10-09 22:25:19 -0700

    Update BootSector.asm

--- Source/x86/BootSector.asm
+++ Source/x86/BootSector.asm
@@ -15,7 +15,7 @@ SectorSize      EQU 0x12
 
 ;OFFSET 00
 ENTRYPOINT:
-        JMP 0x0:CANON_CSIP
+        JMP     0x0:CANON_CSIP
         NOP
 ;OFFSET 03
 b08_OEM         DB 'Yggdrasi'
@@ -59,110 +59,110 @@ FileSysID       DB 'FAT12   '
 CANON_CSIP:
     ;CONFIGURE STACK
         CLI                             ;DISABLE INTERRUPTS
-        MOV AX, 0x7C00 + 0x0100         ;INITIALIZE SS:SP
-        MOV SS, AX
-        MOV SP, 0x1000
+        MOV     AX, 0x7C00 + 0x0100         ;INITIALIZE SS:SP
+        MOV     SS, AX
+        MOV     SP, 0x1000
         STI
     ;
-        PUSH CS
-        POP AX
-        MOV DS, AX
-        MOV ES, AX
-        MOV [Boot_Drive], DL            ;SAVE BOOT DRIVE NUMBER
+        PUSH    CS
+        POP     AX
+        MOV     DS, AX
+        MOV     ES, AX
+        MOV     [Boot_Drive], DL            ;SAVE BOOT DRIVE NUMBER
     ;PRINT BOOT MESSAGE
-        MOV SI, BootText
-        CALL PRINT
+        MOV     SI, BootText
+        CALL    PRINT
     ;LOAD KERNEL
-        MOV AX, SYSSEG
-        MOV ES, AX
-        CALL KERNEL_LOAD
-        JNC LOAD_GOOD
+        MOV     AX, SYSSEG
+        MOV     ES, AX
+        CALL    KERNEL_LOAD
+        JNC     LOAD_GOOD
 LOAD_FAIL:
     ;PRINT FAIL MESSAGE
-        PUSH CS
-        POP DS
-        MOV SI, BootLoadFail
-        CALL PRINT
-        JMP $
-        INT 0x19
+        PUSH    CS
+        POP     DS
+        MOV     SI, BootLoadFail
+        CALL    PRINT
+        JMP     $
+        INT     0x19
 LOAD_GOOD:
     ;CHECK FOR YGGDRASIL KERNEL SIGNATURE
-        MOV SI, READ_ADDRESS
-        MOV AX, SYSSEG
-        MOV DS, AX
-        MOV DI, SIGNATURE
-        PUSH CS
-        POP ES
-        MOV CX, 0x0009
+        MOV     SI, READ_ADDRESS
+        MOV     AX, SYSSEG
+        MOV     DS, AX
+        MOV     DI, SIGNATURE
+        PUSH    CS
+        POP     ES
+        MOV     CX, 0x0009
 LOAD_CHECK:
         CMPSB
-        JNE LOAD_FAIL
-        DEC CX
-        JCXZ KERNEL_GOOD
-        JMP LOAD_CHECK
+        JNE     LOAD_FAIL
+        DEC     CX
+        JCXZ    KERNEL_GOOD
+        JMP     LOAD_CHECK
 KERNEL_GOOD:
     ;PRINT LOADED MESSAGE
-        PUSH CS
-        POP DS
-        MOV SI, GOOD
-        CALL PRINT
+        PUSH    CS
+        POP     DS
+        MOV     SI, GOOD
+        CALL    PRINT
     ;DETERMINE CPU TYPE
-        CALL CPU_ID
+        CALL    CPU_ID
     ;TRANSFER TO STAGE 2
-        JMP SYSSEG:READ_ADDRESS + 0x0009
+        JMP     SYSSEG:READ_ADDRESS + 0x0009
 KERNEL_LOAD:
 
 
-        MOV AX, ES
-        TEST AX, 0x0FFF
-DIE:    JNE DIE
-        MOV BX, READ_ADDRESS                            ;LOAD DISK TRANSFER ADDRESS
+        MOV     AX, ES
+        TEST    AX, 0x0FFF
+DIE:    JNE     DIE
+        MOV     BX, READ_ADDRESS                            ;LOAD DISK TRANSFER ADDRESS
 READA:
     ;RETURN IF ALL DATA HAS BEEN READ
-        MOV AX, ES
-        CMP AX, ENDSEG
-        JB READB
+        MOV     AX, ES
+        CMP     AX, ENDSEG
+        JB      READB
         CLC
         RET
 READB:
-        MOV AX, SectorSize
-        SUB AX, [SECTORS]
-        MOV CX, AX
-        SHL CX, 0x09
-        ADD CX, BX
-        JNC READC
-        JE READC
-        XOR AX, AX
-        SUB AX, BX
-        SHR AX, 0x09
+        MOV     AX, SectorSize
+        SUB     AX, [SECTORS]
+        MOV     CX, AX
+        SHL     CX, 0x09
+        ADD     CX, BX
+        JNC     READC
+        JE      READC
+        XOR     AX, AX
+        SUB     AX, BX
+        SHR     AX, 0x09
 READC:
-        CALL READ
-        JC READ_FAIL
-        MOV CX, AX
-        ADD AX, [SECTORS]
-        CMP AX, SectorSize
-        JNE READE
-        MOV AX, 0x01
-        SUB AX, [HEAD]
-        JNE READD
-        PUSH AX
-        MOV AX, [TRACK]
-        INC AX
-        MOV [TRACK], AX
-        POP AX
+        CALL    READ
+        JC      READ_FAIL
+        MOV     CX, AX
+        ADD     AX, [SECTORS]
+        CMP     AX, SectorSize
+        JNE     READE
+        MOV     AX, 0x01
+        SUB     AX, [HEAD]
+        JNE     READD
+        PUSH    AX
+        MOV     AX, [TRACK]
+        INC     AX
+        MOV     [TRACK], AX
+        POP     AX
 READD:
-        MOV [HEAD], AX
-        XOR AX, AX
+        MOV     [HEAD], AX
+        XOR     AX, AX
 READE:
-        MOV [SECTORS], AX
-        SHL CX, 0x09
-        ADD BX, CX
-        JNC READA
-        MOV AX, ES
-        ADD AX, 0x1000
-        MOV ES, AX
-        XOR BX, BX
-        JMP READA
+        MOV     [SECTORS], AX
+        SHL     CX, 0x09
+        ADD     BX, CX
+        JNC     READA
+        MOV     AX, ES
+        ADD     AX, 0x1000
+        MOV     ES, AX
+        XOR     BX, BX
+        JMP     READA
 READ_FAIL:
         RET
 
@@ -171,55 +171,55 @@ READ_FAIL:
         HEAD:    DW 0x0000
 
 READ:
-        PUSH AX
-        PUSH BX
-        PUSH CX
-        PUSH DX
-        MOV DX, [TRACK]
-        MOV CX, [SECTORS]
-        INC CX
-        MOV CH, DL
-        MOV DX, [HEAD]
-        MOV DH, DL
-        AND DX, 0x0100
-        MOV AH, 0x02
-        INT 0x13
-        JC READ_BAD
-        POP DX
-        POP CX
-        POP BX
-        POP AX
+        PUSH    AX
+        PUSH    BX
+        PUSH    CX
+        PUSH    DX
+        MOV     DX, [TRACK]
+        MOV     CX, [SECTORS]
+        INC     CX
+        MOV     CH, DL
+        MOV     DX, [HEAD]
+        MOV     DH, DL
+        AND     DX, 0x0100
+        MOV     AH, 0x02
+        INT     0x13
+        JC      READ_BAD
+        POP     DX
+        POP     CX
+        POP     BX
+        POP     AX
         CLC
         RET
 READ_BAD:
-        XOR AX, AX
-        MOV DX, AX
-        INT 0x13
-        POP DX
-        POP CX
-        POP BX
-        POP AX
+        XOR     AX, AX
+        MOV     DX, AX
+        INT     0x13
+        POP     DX
+        POP     CX
+        POP     BX
+        POP     AX
         STC
         RET
 
 ;PRINT A ZERO-TERMINATED STRING
 PRINT:
         LODSB                                           ;LOAD BYTE
-        OR AL, AL                                       ;TEST FOR ZERO
-        JZ PRINTA                                       ;RETURN IF ZERO
-        MOV BX, 0x000F                                  ;CHARACTER ATTRIBUTE
-        MOV AH, 0x0E                                    ;TTY MODE
-        INT 0x10                                        ;PRINT CHARACTER
-        JMP PRINT                                       ;REPEAT UNTIL FINISHED
+        OR      AL, AL                                       ;TEST FOR ZERO
+        JZ      PRINTA                                       ;RETURN IF ZERO
+        MOV     BX, 0x000F                                  ;CHARACTER ATTRIBUTE
+        MOV     AH, 0x0E                                    ;TTY MODE
+        INT     0x10                                        ;PRINT CHARACTER
+        JMP     PRINT                                       ;REPEAT UNTIL FINISHED
 PRINTA:
         RET                                             ;RETURN
 
 ;PRINT A CRLF
 PRINTCRLF:
-        MOV AX, 0x0E0A
-        INT 0x10
-        MOV AX, 0x0E0D
-        INT 0x10
+        MOV     AX, 0x0E0A
+        INT     0x10
+        MOV     AX, 0x0E0D
+        INT     0x10
         RET
 
 INCLUDE 'CPU_UTIL.ASM'
